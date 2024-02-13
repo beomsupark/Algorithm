7a1643d02ec9f20170969734edc6f41a9bd30350
beomsupark <77788646+beomsupark@users.noreply.github.com>
2024-02-13 14:31:20 +0900

    [Silver III] Title: 이전 순열, Time: 396 ms, Memory: 19532 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+/**
+ * [S3] 10972
+ * @author 박범수
+ * 수행시간 : 	384ms
+ * 메모리 : 18408kb
+ * 
+ * 수업시간에 배운 nextPermutation 방식을 이용하여 구현하여 해결하였다.
+ * */
+
+public class Main{
+	static int N;
+	static int[] list;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N= Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		list= new int[N];
+		for(int i=0;i<N;i++) {
+			list[i]=Integer.parseInt(st.nextToken());
+		}
+		nextPermutation();
+	}
+	
+	public static void nextPermutation() {
+		int index=-1;
+		for(int i=N-2;i>=0;i--) {
+			if(list[i]>list[i+1]) {
+				index=i;
+				break;
+			}
+		}
+		//System.out.println(index);
+		if(index==-1) {
+			System.out.println("-1");
+			return;
+		}
+		
+		for(int i=N-1;i>index;i--) {
+			if(list[index]>list[i]) {
+				swap(index,i);
+				break;
+			}
+		}
+		index++;
+		int end = N-1;
+		while(index<end) {
+			swap(index++,end--);
+		}
+		
+		for(int i=0;i<N;i++) {
+			System.out.print(list[i]+" ");
+		}
+	}
+	
+	public static void swap(int left, int right) {
+		int temp = list[left];
+		list[left]=list[right];
+		list[right]=temp;
+	}
+}
